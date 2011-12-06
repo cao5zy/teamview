3d1f31b30b2021403e44e43109a175ffbeedd75f
Administrator <Administrator@czy-vm>
2011-12-06 22:21:50 +0800

    1.0.0.26 add logTypeEnum enum by czy

---
+++ BugInfo.Common/LogTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BugInfo.Common
+{
+    public enum LogTypeEnum
+    {
+        None = 0,
+        Submit = 1,
+        CodeFeedback = 2,
+    }
+}
