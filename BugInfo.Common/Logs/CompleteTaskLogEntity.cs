213bd6c8102f3261ef59f50a747a431d17daa1dd
czy <czy@czy-PC>
2012-02-11 23:52:41 +0800

    1.0.1.25 add files

---
+++ BugInfo.Common/Logs/CompleteTaskLogEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamView.Common.Logs
+{
+    public class CompleteTaskLogEntity
+    {
+        public string ItemId { get; set; }
+        public int Order { get; set; }
+        public string Dealman { get; set; }
+        public int Estimate { get; set; }
+        public decimal Burned { get; set; }
+        public string Description { get; set; }
+        public DateTime CompleteTime { get; set; }
+    }
+}
