b32cd28d820266c9a72181f56985bd54562399d8
czy <czy@czy-PC.active.local>
2012-04-10 23:40:55 +0800

    add importer project

---
+++ Importer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
