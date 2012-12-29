4e0ee15014a232df66027d033aeb3bf0757b28a8
hg <hg@localhost>
2012-12-29 23:36:50 +0800

    add abstract

---
+++ TeamView.Data.Abstract/Entity/ProgrammerPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamView.Data.Abstract.Entities
+{
+    public class ProgrammerPoint
+    {
+        public string EstimatedBy { get; set; }
+
+        public string Assignee { get; set; }
+
+        public string EstimatedLevel { get; set; }
+
+        public DateTime EstimatedTime { get; set; }
+    }
+}
---
+++ TeamView.Data.Abstract/Entity/TaskLogEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamView.Data.Abstract.Entities
+{
+    public sealed class TaskLogEntity
+    {
+        public string ItemId { get; set; }
+        public string Dealman { get; set; }
+        public int Estimate { get; set; }
+        public int Burned { get; set; }
+        public string Description { get; set; }
+        public DateTime CompleteTime { get; set; }
+        public string Version { get; set; }
+        public string Status { get; set; }
+    }
+}
