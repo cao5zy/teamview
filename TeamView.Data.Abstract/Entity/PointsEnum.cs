ad87c163d34585f9c5b8664ecc08a028d851b4d4
hg <hg@localhost>
2012-12-29 23:36:50 +0800

    add abstract

---
+++ TeamView.Data.Abstract/Entity/PointsEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamView.Data.Abstract.Entities
+{
+    public enum PointsEnum
+    {
+        Basic = 1,
+        BasicPlus = 2,
+        Median = 3,
+        MedianPlus = 4, 
+        High = 5,
+        HighPlus = 6
+    }
+}
