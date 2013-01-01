fef765067573169745464345c858af540be642b2
hg <hg@localhost>
2013-01-01 17:38:23 +0800

    add interfaces to abstracts

---
+++ TeamView.Data.Abstract/IBugInfoRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeamView.Data.Abstract.Entities;
+
+namespace TeamView.Data.Abstract
+{
+    public interface IBugInfoRepository
+    {
+        BugInfoEntity GetItem(string itemId);
+
+        void UpdateItem(BugInfoEntity item);
+
+        long ? GetCurrentKeyValue(string keyName);
+
+        void UpdateKeyValue(string keyName, long newVal);
+
+        void InsertKeyValue(string keyName, long val);
+
+        void SaveDoc(string itemId,byte[] stream);
+
+        byte[] LoadDoc(string itemId);
+
+        bool IsLargestOrder(string itemId);
+
+        bool CheckDealManStatus(string dealMan, string bugStatus);
+
+        void AddLog(string bugNum, string desc, int logTypeId);
+
+        bool CheckTimeStamp(string itemId, DateTime timeStamp);
+
+        bool HasLog(string bugNum);
+    }
+}
---
+++ TeamView.Data.Abstract/IQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeamView.Data.Abstract.Entities;
+
+namespace TeamView.Data.Abstract
+{
+    public interface IQuery
+    {
+        IEnumerable<BugInfoEntity> QueryByParameters(
+            IEnumerable<string> programmers,
+            string bugNum,
+            string version,
+            string description,
+            IEnumerable<int> selectedPriorities,
+            IEnumerable<string> selectedStates);
+
+        TaskLogEntity[] QueryTasks(string programmer, DateTime searchStart, DateTime searchEnd);
+
+        int CountFeedbacks(string itemId);
+    }
+}
