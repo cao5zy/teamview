4ecc36e725f76e46814d0310be411badbc1f54ec
hg <hg@localhost>
2013-01-01 19:45:11 +0800

    implement iquery

---
+++ TeamView.Data.Sql/SqlBugInfoQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamView.Data.Sql
+{
+    class SqlBugInfoQuery : TeamView.Data.Abstract.IQuery
+    {
+        public IEnumerable<Abstract.Entities.BugInfoEntity> QueryByParameters(IEnumerable<string> programmers, string bugNum, string version, string description, IEnumerable<int> selectedPriorities, IEnumerable<string> selectedStates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Abstract.Entities.TaskLogEntity[] QueryTasks(string programmer, DateTime searchStart, DateTime searchEnd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CountFeedbacks(string itemId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
