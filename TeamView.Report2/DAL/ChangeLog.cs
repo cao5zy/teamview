ff66667419096cfac116d65cd858b1795f3d7112
cao <cao@localhost>
2013-03-03 10:37:24 +0800

    upgrade to .net 4

---
+++ TeamView.Report2/DAL/ChangeLog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamView.Report2.DAL
+{
+    class ChangeLog : Interfaces.IChangeLog
+    {
+    }
+}
--- TeamView.Report2/Program.cs
+++ TeamView.Report2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Autofac;
+using Dev3Lib.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -15,6 +17,13 @@ namespace TeamView.Report2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            DependencyFactory.SetContainer(() => {
+                ContainerBuilder builder = new ContainerBuilder();
+                builder.RegisterType<TeamView.Report2.DAL.BugInfo>().As<TeamView.Report2.DAL.Interfaces.IBugInfo>();
+                builder.RegisterType<Report2.DAL.ChangeLog>().As<Report2.DAL.Interfaces.IChangeLog>();
+
+                return builder.Build();
+            });
             Application.Run(new ReportForm());
         }
     }
