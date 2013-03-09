29d7155383e8572579fac732aec61bc86f427329
cao <cao@localhost>
2013-03-10 07:30:26 +0800

    add unit test to changeloglogic

--- TeamView.Test/ChangeLogLogicTests.cs
+++ TeamView.Test/ChangeLogLogicTests.cs
@@ -4,6 +4,10 @@ using Rhino.Mocks;
 using TeamView.Report2.DAL.Interfaces;
 using TeamView.Report2.Entities;
 using System.Collections.Generic;
+using Dev3Lib;
+using Autofac;
+using TeamView.Report2.BLL;
+using Rhino.Mocks.Constraints;
 
 namespace TeamView.Test
 {
@@ -16,8 +20,8 @@ namespace TeamView.Test
             string bugNum = "no";
             DateTime startDate = DateTime.Now;
             DateTime endDate = DateTime.Now.AddDays(3);
-            int logStartType = 1;
-            int logEndType = 2;
+            int logStartType = 2;
+            int logEndType = 3;
 
             MockRepository mock = new MockRepository();
             var changeLogDal = mock.StrictMock<IChangeLog>();
@@ -25,12 +29,98 @@ namespace TeamView.Test
             using (mock.Record())
             {
                 changeLogDal.GetLogs(bugNum, startDate, endDate);
+                LastCall.Constraints(Is.Anything(), Is.Anything(), Is.Anything());
                 LastCall.Return(new List<ChangeLogEntity>{ 
                     new ChangeLogEntity{
                         LogTypeID = logStartType,
                         CreateDate = startDate,
-                    }
+                    },
+                    new ChangeLogEntity{
+                        LogTypeID = logEndType,
+                        CreateDate = startDate.AddHours(1),
+                    },
+                    new ChangeLogEntity{
+                        LogTypeID = logStartType,
+                        CreateDate = startDate.AddHours(2),
+                    },
+                    new ChangeLogEntity{
+                        LogTypeID = logEndType,
+                        CreateDate = startDate.AddHours(3),
+                    },
                 });
+            }
+
+            DependencyFactory.SetContainer(() =>
+            {
+                ContainerBuilder builder = new ContainerBuilder();
+                builder.RegisterType<ChangeLogLogic>();
+                builder.RegisterInstance(changeLogDal).As<IChangeLog>();
+
+                return builder.Build();
+            });
+
+            using (DependencyFactory.BeginScope())
+            {
+                var changeLogLogic = DependencyFactory.Resolve<ChangeLogLogic>();
+
+                var duration = changeLogLogic.CalculateCurrentBurnedMins(bugNum, startDate, endDate);
+
+                Assert.AreEqual(120, duration);
+            }
+        }
+
+        [TestMethod]
+        public void CalculateCurrentBurnedMins_TheFirstIsEndLogType_ReturnList()
+        {
+            string bugNum = "no";
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Now.AddDays(3);
+            int logStartType = 2;
+            int logEndType = 3;
+
+            MockRepository mock = new MockRepository();
+            var changeLogDal = mock.StrictMock<IChangeLog>();
+
+            using (mock.Record())
+            {
+                changeLogDal.GetLogs(bugNum, startDate, endDate);
+                LastCall.Constraints(Is.Anything(), Is.Anything(), Is.Anything());
+                LastCall.Return(new List<ChangeLogEntity>{ 
+                    new ChangeLogEntity{
+                        LogTypeID = logEndType,
+                        CreateDate = startDate,
+                    },
+                    new ChangeLogEntity{
+                        LogTypeID = logStartType,
+                        CreateDate = startDate.AddHours(1),
+                    },
+                    new ChangeLogEntity{
+                        LogTypeID = logEndType,
+                        CreateDate = startDate.AddHours(2),
+                    },
+                    new ChangeLogEntity{
+                        LogTypeID = logStartType,
+                        CreateDate = startDate.AddHours(3),
+                    },
+                });
+            }
+
+            DependencyFactory.SetContainer(() =>
+            {
+                ContainerBuilder builder = new ContainerBuilder();
+                builder.RegisterType<ChangeLogLogic>();
+                builder.RegisterInstance(changeLogDal).As<IChangeLog>();
+
+                return builder.Build();
+            });
+
+            using (DependencyFactory.BeginScope())
+            {
+                var changeLogLogic = DependencyFactory.Resolve<ChangeLogLogic>();
+
+                var duration = changeLogLogic.CalculateCurrentBurnedMins(bugNum, startDate, endDate);
+
+                Assert.AreEqual(60, duration);
             }
         }
     }
